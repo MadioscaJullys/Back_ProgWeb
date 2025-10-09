b3a3e6eb9a569557ad46a8cb4f9b5d86af024d4d
jully <madison.jully@estudantes.ifg.edu.br>
2025-10-09 20:30:38 -0300

    commit2

---
+++ role/role_repository.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+from . import role_model
+
+def get_role_by_name(db: Session, name: str):
+    return db.query(role_model.Role).filter(role_model.Role.name == name).first()
+
+# Funções básicas do CRUD para Role
+def get_all_roles(db: Session):
+    return db.query(role_model.Role).all()
+
+def create_role(db: Session, role: role_model.RoleCreate):
+    db_role = role_model.Role(name=role.name)
+    db.add(db_role)
+    db.commit()
+    db.refresh(db_role)
+    return db_role
